8774a8ca87c2d23a059d94f349d236a58d521a8b
Fritozz-105 <zengz1@ufl.edu>
2023-10-22 21:30:22 -0400

    This is my first addition to my repo.

---
+++ password_encoder.py
@@ -0,0 +1,15 @@
+def print_menu():
+    print('Menu')
+    print('-' * 13)
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+
+
+def main():
+    print_menu()
+
+
+if __name__ == '__main__':
+    main()
