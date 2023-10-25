059ec0bc92767e84e67863a0869a99696469514e
Fritozz-105 <zengz1@ufl.edu>
2023-10-25 09:06:05 -0400

    This is the def main and def encode for the project

--- password_encoder.py
+++ password_encoder.py
@@ -1,4 +1,5 @@
 def print_menu():
+    # Prints the menu with options to encode a password, decode the password, and quit.
     print('Menu')
     print('-' * 13)
     print('1. Encode')
@@ -7,8 +8,38 @@ def print_menu():
     print()
 
 
+def encode_pw(password):
+    # Takes the user's input and uses a for loop to turn the input to an integer to add 3 to and then turns it back into
+    # a string where it is added to the user_password return.
+    user_password = ''
+    for i in range(len(password)):
+        if (int(password[i]) + 3) > 9:
+            user_password = user_password + str((int(password[i]) + 3) - 10)
+        else:
+            user_password = user_password + str(int(password[i]) + 3)
+    return user_password
+
+
 def main():
-    print_menu()
+    try:
+        user_choice = 1
+        while user_choice != 3:
+            # While loop that runs until the user selects option 3.
+            print_menu()
+            user_choice = int(input('Please enter an option: '))
+            if user_choice == 1:
+                user_password = input('Please enter your password to encode: ')
+                print('Your password has been encoded and stored!')
+                print()
+                encoded_password = encode_pw(user_password)
+            elif user_choice == 2:
+                print(f'The encoded password is {encoded_password}, and the original password is '
+                      f'{user_password}.')
+                print()
+    except UnboundLocalError as excpt:
+        print(f'Please set a password first, {excpt}')
+    except:
+        print('Unknown error occurred')
 
 
 if __name__ == '__main__':
